dc3ec30ad7c3a26b25d896bd9e05bde3823606a6
hanaematsuo <hanae.matsuo.k347@dirbato.co.jp>
2024-09-11 16:36:44 +0900

    最初のコミット

--- OopBasic/src/company/Employee.java
+++ OopBasic/src/company/Employee.java
@@ -1,9 +1,9 @@
 package company;
 
-public class Employee {
+public abstract class Employee implements Workable {
     
     protected final String name;
-    private final Department department;
+    protected final Department department;
     private final String position;
     private final int employeeId;
     
@@ -14,16 +14,23 @@ public class Employee {
         this.employeeId = employeeId;
     }
     
+    
     public void report(int times) {
         System.out.println(times + "回目の報告をします。役職：" + position + "、 名前：" + name);
     }
+    
     
     public void report() {
         report(1);
     }
     
-    public void joinMeeting() {
-        department.meeting();
-        System.out.println("→上記の会議に参加します。部署：" + department.getName() + "名前:" + name);
+    
+    public abstract void joinMeeting() ;
+    
+    
+    
+    @Override
+    public void work() {
+        System.out.println("正社員として働きます。名前：" + name + slogan);
     }
 }
--- OopBasic/src/company/ObjectSample.java
+++ OopBasic/src/company/ObjectSample.java
@@ -4,20 +4,46 @@ public class ObjectSample {
 
     public static void main(String[] args) {
         
-        var department = new Department("営業部", "xx", 1000000);
+        var salesDepartment = new Department("営業部", "xx", 1000000);
         var employee = new Employee("鈴木", department, "課長", 100);
         
-        employee.report();
-        employee.report(2);
-        employee.joinMeeting();
+        sales.report();
+        sales.report(2);
+        sales.joinMeeting();
         
         System.out.println("");
         
         var devDepartment = new Department("開発部", "yy", 0);
-        var engineer = new Employee("田中", "devDepartment", "一般社員", 88);
+        var engineer = new Engineer("田中", "devDepartment", "一般社員", 88, "Java");
         
         engineer.report();
         engineer.joinMeeting();
+        engineer.developSoftware();
+        
+        
+        System.out.println("");
+        
+        Employee projectManager = new Engineer("佐藤", devDepartment, "PM", 99, "Java");
+        
+        
+        projectManager.report();
+        projectManager.joinMeeting();
+        if (projectManager instanceof Engineer) {
+            
+            ((Engineer) projectManager).developSoftware();
+        }
+        
+        
+        System.out.println("");
+        
+        var parttimeWorker = new ParttimeWorker("太田", salesDepartment);
+        
+        
+        ((Workable) employee).work();
+        ((Workable) engineer).work();
+        ((Workable) projectManager).work();
+        ((Workable) parttimeWorker).work();
     }
 
+    
 }
