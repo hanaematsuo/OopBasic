647d0175d2a53bc0af624547c444bd6ac7de5c09
hanaematsuo <hanae.matsuo.k347@dirbato.co.jp>
2024-09-11 15:31:43 +0900

    最初のコミット

---
+++ OopBasic/src/company/Employee.java
@@ -0,0 +1,29 @@
+package company;
+
+public class Employee {
+    
+    protected final String name;
+    private final Department department;
+    private final String position;
+    private final int employeeId;
+    
+    public Employee(String name, Department department, String position, int employeeId) {
+        this.name = name;
+        this.department = department;
+        this.position = position;
+        this.employeeId = employeeId;
+    }
+    
+    public void report(int times) {
+        System.out.println(times + "回目の報告をします。役職：" + position + "、 名前：" + name);
+    }
+    
+    public void report() {
+        report(1);
+    }
+    
+    public void joinMeeting() {
+        department.meeting();
+        System.out.println("→上記の会議に参加します。部署：" + department.getName() + "名前:" + name);
+    }
+}
---
+++ OopBasic/src/company/ObjectSample.java
@@ -0,0 +1,23 @@
+package company;
+
+public class ObjectSample {
+
+    public static void main(String[] args) {
+        
+        var department = new Department("営業部", "xx", 1000000);
+        var employee = new Employee("鈴木", department, "課長", 100);
+        
+        employee.report();
+        employee.report(2);
+        employee.joinMeeting();
+        
+        System.out.println("");
+        
+        var devDepartment = new Department("開発部", "yy", 0);
+        var engineer = new Employee("田中", "devDepartment", "一般社員", 88);
+        
+        engineer.report();
+        engineer.joinMeeting();
+    }
+
+}
